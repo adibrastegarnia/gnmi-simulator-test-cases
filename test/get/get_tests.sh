2c7aefc2861db83ccc9a0912af11a7f9125a1d38
adibrastegarnia <arastega@purdue.edu>
2019-06-26 16:35:58 -0700

    update get tests with more test cases

--- test/get/get_tests.sh
+++ test/get/get_tests.sh
@@ -13,6 +13,7 @@ fi
 
 
 case "$testId" in
+  ## Get "clock" config value with type CONFIG
   "test1")
     gnmi_cli -address localhost:10161 -get \
     -proto "type:1, path: <elem: <name:'system'> elem:<name:'clock'>>" \
@@ -21,6 +22,7 @@ case "$testId" in
     -client_key $ONOS_SIMULATORS_ROOT/pkg/certs/client1.key \
     -ca_crt $ONOS_SIMULATORS_ROOT/pkg/certs/onfca.crt
     ;;
+    ## Get "clock" config value with type STATE
    "test2")
     gnmi_cli -address localhost:10161 -get \
     -proto "type:2, path: <elem: <name:'system'> elem:<name:'clock'>>" \
@@ -28,6 +30,7 @@ case "$testId" in
     -client_crt $ONOS_SIMULATORS_ROOT/pkg/certs/client1.crt \
     -client_key $ONOS_SIMULATORS_ROOT/pkg/certs/client1.key \
     -ca_crt $ONOS_SIMULATORS_ROOT/pkg/certs/onfca.crt;;
+    ## Get "clock" config value with type ALL
     "test3")
     gnmi_cli -address localhost:10161 -get \
     -proto "type:0, path: <elem: <name:'system'> elem:<name:'clock'>>" \
@@ -35,6 +38,7 @@ case "$testId" in
     -client_crt $ONOS_SIMULATORS_ROOT/pkg/certs/client1.crt \
     -client_key $ONOS_SIMULATORS_ROOT/pkg/certs/client1.key \
     -ca_crt $ONOS_SIMULATORS_ROOT/pkg/certs/onfca.crt;;
+    ## Get All Config leaves under /system
     "test4")
     gnmi_cli -address localhost:10161 -get \
     -proto "type:1, path: <elem: <name: 'system'>>" \
@@ -42,9 +46,50 @@ case "$testId" in
     -client_crt $ONOS_SIMULATORS_ROOT/pkg/certs/client1.crt \
     -client_key $ONOS_SIMULATORS_ROOT/pkg/certs/client1.key \
     -ca_crt $ONOS_SIMULATORS_ROOT/pkg/certs/onfca.crt;;
+    ## Get All State Leaves under /system
     "test5")
     gnmi_cli -address localhost:10161 -get \
     -proto "type:2, path: <elem: <name: 'system'>>" \
+    -timeout 5s -alsologtostderr \
+    -client_crt $ONOS_SIMULATORS_ROOT/pkg/certs/client1.crt \
+    -client_key $ONOS_SIMULATORS_ROOT/pkg/certs/client1.key \
+    -ca_crt $ONOS_SIMULATORS_ROOT/pkg/certs/onfca.crt;;
+    ## Get All config leaves under /system/openflow/controllers/
+    "test6")
+    gnmi_cli -address localhost:10161 -get \
+    -proto "type:1, path: <elem: <name: 'system'> elem: <name: 'openflow'> elem: <name: 'controllers'>>" \
+    -timeout 5s -alsologtostderr \
+    -client_crt $ONOS_SIMULATORS_ROOT/pkg/certs/client1.crt \
+    -client_key $ONOS_SIMULATORS_ROOT/pkg/certs/client1.key \
+    -ca_crt $ONOS_SIMULATORS_ROOT/pkg/certs/onfca.crt;;
+    ## Get All state leaves under /system/openflow/controllers
+    "test7")
+    gnmi_cli -address localhost:10161 -get \
+    -proto "type:2, path: <elem: <name: 'system'> elem: <name: 'openflow'> elem: <name: 'controllers'>>" \
+    -timeout 5s -alsologtostderr \
+    -client_crt $ONOS_SIMULATORS_ROOT/pkg/certs/client1.crt \
+    -client_key $ONOS_SIMULATORS_ROOT/pkg/certs/client1.key \
+    -ca_crt $ONOS_SIMULATORS_ROOT/pkg/certs/onfca.crt;;
+    ## Get All config leaves under /system/config
+    "test8")
+    gnmi_cli -address localhost:10161 -get \
+    -proto "type:1, path: <elem: <name:'system'> elem:<name:'config'>>" \
+    -timeout 5s -alsologtostderr \
+    -client_crt $ONOS_SIMULATORS_ROOT/pkg/certs/client1.crt \
+    -client_key $ONOS_SIMULATORS_ROOT/pkg/certs/client1.key \
+    -ca_crt $ONOS_SIMULATORS_ROOT/pkg/certs/onfca.crt;;
+    ## Get All Config values under the root
+    "test9")
+     gnmi_cli -address localhost:10161 -get \
+    -proto "type:1" \
+    -timeout 5s -alsologtostderr \
+    -client_crt $ONOS_SIMULATORS_ROOT/pkg/certs/client1.crt \
+    -client_key $ONOS_SIMULATORS_ROOT/pkg/certs/client1.key \
+    -ca_crt $ONOS_SIMULATORS_ROOT/pkg/certs/onfca.crt;;
+    ## Get All State values under the root
+    "test10")
+    gnmi_cli -address localhost:10161 -get \
+    -proto "type:2" \
     -timeout 5s -alsologtostderr \
     -client_crt $ONOS_SIMULATORS_ROOT/pkg/certs/client1.crt \
     -client_key $ONOS_SIMULATORS_ROOT/pkg/certs/client1.key \
@@ -54,6 +99,5 @@ case "$testId" in
     ;;
 esac
 
-# Test Case 5: Get All State Leaves under /system
  
 
